74c41af659256aa1a6aecf35242423d93f49180e
royweb <xytank@gmail.com>
2019-11-29 13:55:21 -0500

    Set up router

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const http = require('http');
 
 const PORT = process.env.PORT || 8000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 
 // instance of socket.io
 const io = socktio(server);
+
+// use it as middleware
+app.use(router);
 
 server.listen(PORT, () => console.log("Server is running on " + PORT));
