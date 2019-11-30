1fd7cad9125c39a8d245cc6b0dedc96f154c3863
royweb <xytank@gmail.com>
2019-11-30 11:53:50 -0500

    Add helper functions

---
+++ users.js
@@ -0,0 +1,37 @@
+// helper functions
+const addUser = ({ id, name, room }) => {
+    // remove white space and make all the data lowercase
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    // make sure the user is different
+    const existingUser = users.find(user => user.room === room && user.name === name);
+
+    if(existingUser){
+        return { error: "Username already exist" };
+    }
+
+    const user = { id, name, room};
+
+    // add the user to the chatroom
+    users.push(user);
+
+    return { user };
+}
+
+const removeUser = id => {
+    const index = users.findIndex(user => user.id === id);
+
+    if(index !== 1){
+        // remove the user from the chatroom
+        return users.splice(index, 1)[0];
+    }
+}
+
+// find the user
+const getUser = id => users.find(user => user.id === id);
+
+// get all the user from that chatroom
+const getUsersInRoom = room => users.filter(user => user.room === room);
+
+module.exports = { addUser,  removeUser, getUser, getUsersInRoom };
