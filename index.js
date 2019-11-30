d6617c06153162314347e312553b1cf5ca45fb8a
royweb <xytank@gmail.com>
2019-11-30 13:24:09 -0500

    Use socket.on() to send a message to all user in that chatroom

--- index.js
+++ index.js
@@ -37,10 +37,18 @@ io.on("connection", (socket) => {
         callback();
     })
 
+    socket.on("sendMessage", (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit("message", { user: user.name, text: message});
+
+        callback();
+    });
+
     // user that left the chat room
     socket.on("disconnect", () => {
         console.log("User has left");
-    })
+    });
 })
 
 // use it as middleware
